8aa4fa3c71ef7ac16bab0d7cd6320ff94a3837e6
Atul Kumar Jha <atuljha21542@gmail.com>
2025-11-07 19:22:45 +0530

    All setup configured

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,20 @@
- import mongoose from "mongoose";
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance= await mongoose.connect
-        (`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: $ 
-            {connectionInstance.connection.host}` );
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
 
+    console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    
+    return connectionInstance;
 
-    } catch (error) {
-        console.log("MONGODB connection error", error);
-        process.exit(1)
-    }
-}
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import express from "express";
 import connectDB from "./db/index.js";
 
-dotenv.config({
-    path: './env'
-})
+dotenv.config({ path: "./.env" }); // or './env' if that’s your actual filename
 
-connectDB()
-.then(() => {
-    application.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port :$ {process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!!", err);
-})
+const app = express();
+const PORT = process.env.PORT || 8000;
 
+app.get("/", (req, res) => res.send("Hello World!"));
 
+const startServer = async () => {
+  try {
+    const connectionInstance = await connectDB();
+    console.log(`Database connected to host: ${connectionInstance.connection.host}`);
 
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  } catch (err) {
+    console.error("MONGO db connection failed !!!", err);
+    process.exit(1);
+  }
+};
+
+startServer();
 
 
 
